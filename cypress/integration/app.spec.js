709e4c6c046814af015883466e8c2f85bf3ce28d
lei <oizhaolei@gmail.com>
2022-06-01 13:43:06 +0900

    v3??

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,18 +3,13 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
-describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+describe("Navigation", () => {
+  it("should navigate to the about page", () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
+    cy.visit("http://localhost:3000/");
 
     // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click()
-
-    // The new url should include "/about"
-    cy.url().should('include', '/about')
-
-    // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('About Page')
-  })
-})
+    cy.get('input[name="email"]').type("lei.zhao@sun-asterisk.com");
+    cy.get("button").contains("Register").click();
+  });
+});
